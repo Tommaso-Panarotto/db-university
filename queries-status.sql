a9878694c674b9d37a76e9bb124f01f52e66bab2
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-16 17:53:07 +0200

    departments

--- queries-status.sql
+++ queries-status.sql
@@ -29,3 +29,8 @@ WHERE `date` = "2020-06-20" AND `hour` > "14:00:00";
 SELECT *
 FROM `degrees`
 WHERE `level` = "magistrale";
+
+--7. Da quanti dipartimenti è composta l'università? (12)
+
+SELECT *
+FROM `departments`;
