13ef62117f9d9e4724b7d2313c38643e4e24b6bd
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-16 17:52:08 +0200

    degrees

--- queries-status.sql
+++ queries-status.sql
@@ -23,3 +23,9 @@ WHERE `year` = 1 AND `period` = "I semestre";
 SELECT *
 FROM `exams`
 WHERE `date` = "2020-06-20" AND `hour` > "14:00:00";
+
+--6. Selezionare tutti i corsi di laurea magistrale (38)
+
+SELECT *
+FROM `degrees`
+WHERE `level` = "magistrale";
